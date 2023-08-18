3aa378cb04c8d3ed012e0dd605d5fe14c5844264
cas291 <101031743+cas291@users.noreply.github.com>
2023-08-18 16:50:34 +0800

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Typecho Blog Platform
+ *
+ * @copyright  Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license    GNU General Public License 2.0
+ * @version    $Id$
+ */
+
+/** 定义根目录 */
+define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
+
+/** 定义插件目录(相对路径) */
+define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
+
+/** 定义模板目录(相对路径) */
+define('__TYPECHO_THEME_DIR__', '/usr/themes');
+
+/** 后台路径(相对路径) */
+define('__TYPECHO_ADMIN_DIR__', '/admin/');
+
+/** 设置包含路径 */
+@set_include_path(get_include_path() . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR .
+__TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
+
+/** 载入API支持 */
+require_once 'Typecho/Common.php';
+
+/** 程序初始化 */
+Typecho_Common::init();
+
+/** 定义数据库参数 */
+$db = new Typecho_Db('Pdo_Pgsql', 'typecho_');
+$db->addServer(array (
+  'host' => 'hansken.db.elephantsql.com',
+  'user' => 'tircbfal',
+  'password' => 'jPq1CqP0jfqrg2PPUf71gtRj3OCb1hFG',
+  'charset' => 'utf8',
+  'port' => '5432',
+  'database' => 'tircbfal',
+), Typecho_Db::READ | Typecho_Db::WRITE);
+Typecho_Db::set($db);
